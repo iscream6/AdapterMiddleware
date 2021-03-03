2f38704ef89f15686064d5b1d57f54c10f7b84a8
이재영 <iscream6@gmail.com>
2021-03-03 16:09:38 +0900

    기흥힐스테이트 Point 조회 추가

---
+++ NpmAdapter/Payload/CommaxDaelim/Response/ResponseCmdRemainPointData.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json.Linq;
+using NexpaAdapterStandardLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NpmAdapter.Payload
+{
+    class ResponseCmdRemainPointData : IPayload
+    {
+        public string point { get; set; }
+
+        public void Deserialize(JObject json)
+        {
+            point = Helper.NVL(json["point"]);
+        }
+
+        public byte[] Serialize()
+        {
+            return ToJson().ToByteArray(SysConfig.Instance.HomeNet_Encoding);
+        }
+
+        public JObject ToJson()
+        {
+            JObject json = new JObject();
+            json["point"] = Helper.NVL(point);
+            return json;
+        }
+    }
+}
