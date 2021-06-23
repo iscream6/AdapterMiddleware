94e91eeaaf8e5c6dfef1b14815388806c4bb925b
이재영 <iscream6@gmail.com>
2021-06-23 10:08:21 +0900

    Kakao 연동, 폴더 정리

---
+++ NpmAdapter/Pipe/KakaoPipe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NpmAdapter
+{
+    class KakaoPipe : AbstractPipe , IDisposable
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool GeneratePipe()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool StartAdapter(AdapterType type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool StopAdapter(AdapterType type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
