17c29dea7e4c21dbb42e4006a220aa37a1cfe5fb
이재영 <iscream6@gmail.com>
2021-09-03 13:04:41 +0900

    WPF Page Resouce 추가

---
+++ Application/NexpaConnectWpfApp/Common/PageResource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NexpaConnectWpfApp.Common
+{
+    class PageResource
+    {
+        public static Uri MainUri = new Uri("/View/MainGate.xaml", UriKind.RelativeOrAbsolute);
+        public static Uri OptionUri = new Uri("/View/Test.xaml", UriKind.Relative);
+    }
+}
